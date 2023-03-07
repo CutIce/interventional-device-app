eef81875aacba311e07352528ff14a6cd5bc45f9
CutIce <1031941295@qq.com>
2023-03-07 23:44:24 +0800

    Process finished with exit code -1073741511 (0xC0000139)报错解决方法：在run configuration中的environment variables设置成path=E:\Softwares\qt\qt_5_14\5.14.2\mingw73_64\bin

--- omega7_com/main.cpp
+++ omega7_com/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[]) {
     qDebug() << "start ! \n";
 
     mainwindow w;
+    w.resize(1000,500);
+    w.move(300,150);
+    w.setFocus();
     w.show();
     return a.exec();
 }
